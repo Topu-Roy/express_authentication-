6bb30cf78262334dc0e2be675497525a9473ba11
Topu Roy <topuroy143333@gmail.com.com>
2023-05-04 17:39:12 +0600

    Checking Cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 5051;
@@ -7,9 +8,15 @@ const port = 5051;
 app.use(express.static(path.join(__dirname, "public")));
 // app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("token", "this is a login cookie");
+  res.send("Cookie Set");
 });
 
 app.listen(port, () => {

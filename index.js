494f2b04d3dc52b0dd810c6844c905dd040f27f2
Topu Roy <topuroy143333@gmail.com.com>
2023-05-04 19:45:48 +0600

    Added Login Logout feature with cookies

--- index.js
+++ index.js
@@ -6,17 +6,30 @@ const app = express();
 const port = 5051;
 
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const { token } = req.cookies;
+  if (token) {
+    res.render("logout");
+  } else {
+    res.render("index");
+  }
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/");
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("token", "this is a login cookie");
-  res.send("Cookie Set");
+  res.cookie("token", "this is a login cookie", {
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+  res.redirect("/");
 });
 
 app.listen(port, () => {
